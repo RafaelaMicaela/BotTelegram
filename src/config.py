f6ca54549cdfbcdbd9c21b311fd3ae28bf534599
rafaela <rafaela.micaela.77@gmail.com>
2026-03-12 15:04:31 -0300

    feat: update deploy

--- src/config.py
+++ src/config.py
@@ -1,3 +1,7 @@
-# Configuração para Railway: busca o token do ambiente
 import os
+from dotenv import load_dotenv
+
+# Isso aqui é o que "puxa" as variáveis do arquivo .env para a memória do seu PC
+load_dotenv()
+
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
